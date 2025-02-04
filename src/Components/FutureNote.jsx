456bb15228c7acd6cebcd7b415e5d2b50ad76609
Aastha-2309 <aastha.codes23@gmail.com>
2025-02-04 16:00:06 +0530

    url update

--- src/Components/FutureNote.jsx
+++ src/Components/FutureNote.jsx
@@ -34,7 +34,7 @@ const FutureNote = () => {
 
     try {
       const response = await axios.post(
-        'http://13.233.75.216/api/future-notes',
+        'http://13.233.75.216/api/api/future-notes',
         {
           message,
           date: selectedDate.toISOString(), // Convert to ISO string for consistency
