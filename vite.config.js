9f0f88756d1c0de0f19a9f3cbba039efcb62066c
Aastha-2309 <aastha.codes23@gmail.com>
2025-02-11 00:39:26 +0530

    performance issues sol

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import purgecss from '@fullhuman/postcss-purgecss'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+  ],
+  css: {
+    postcss: {
+      plugins: [
+        purgecss({
+          content: [
+            './index.html', // Scan the HTML files
+            './src/**/*.{js,jsx,ts,tsx}' // Also scan React component files (JSX/TSX)
+          ]
+        })
+      ]
+    }
+  }
 })
